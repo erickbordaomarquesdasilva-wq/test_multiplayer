b9d375ecabc0cec5e5066fa4996471e210230458
erickbordaomarquesdasilva-wq <erickbordaomarquesdasilva@gmail.com>
2025-10-22 22:50:17 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+import { WebSocketServer } from 'ws';
+
+const PORT = process.env.PORT || 8080; // Render vai mapear internamente
+const wss = new WebSocketServer({ port: PORT });
+
+wss.on('connection', ws => {
+    console.log("Jogador conectado");
+
+    ws.on('message', msg => console.log("Mensagem:", msg.toString()));
+});
+
